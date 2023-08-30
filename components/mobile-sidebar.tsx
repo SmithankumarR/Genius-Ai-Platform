ee6c86b408e39fa4a4f5e043e50061e5a805a478
SmithanKumarR <smithankumar404@gmail.com>
2023-08-30 12:50:26 +0530

    DAY-2 :WIP Sidebar section complete

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Menu } from "lucide-react";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Sidebar from "./sidebar";
+import { useState,useEffect } from "react";
+
+const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="p-0" side="left">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
